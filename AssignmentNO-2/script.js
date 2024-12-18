294e7ce2013917ed01191eff724ff9f5d28af5e3
muhammadibrahim-dev <muhammadibrahimmughal35@gmail.com>
2024-12-18 02:31:34 -0800

    complete assignment number 2

--- AssignmentNO-2/script.js
+++ AssignmentNO-2/script.js
@@ -185,3 +185,173 @@ function month() {
     }
 }
  month()
+ // NO#13
+ function countamount(amounts) {
+    let notes = [1000,500,100,50,30,20,10];
+     let notecounts = {};
+     for (let note of notes)
+        if ( amounts > note) {
+             let count = Math.floor(amounts / note);
+            notecounts[note]=count;
+            amounts -= count * note;
+        } 
+          return(
+            notecounts
+        )
+        }
+ let amounts =123;
+ let result = countamount(amounts)
+ console.log(result);
+ countamount()
+ // NO#14
+ function angle() {
+    let num1 = prompt("enter left side number");
+    let num2 = prompt("enter right side number");
+    let num3 = prompt("enter bottom side number");
+    let result = 270;
+    let angle = Math.acos((num1**2 + num2**2 - num3))
+    if ( result === angle) {
+        console.log("triangle is valid");
+        
+    } else {
+        console.log("triangle is not valid");
+    }
+ }
+ angle()
+ // NO#15
+ function angle() {
+    let num1 = prompt("enter left side number");
+    let num2 = prompt("enter right side number");
+    let num3 = prompt("enter bottom side number");
+    let result = 270;
+    let angle = Math.acos((num1**2 + num2**2 - num3))
+    if ( result === angle) {
+        console.log("triangle is valid");
+        
+    } else {
+        console.log("triangle is not valid");
+    }
+ }
+ angle()
+//  NO#16
+function checkTriangle(a, b, c) {
+
+    if (a + b <= c || a + c <= b || b + c <= a) {
+        return "Not a triangle";
+    }
+    if (a === b && b === c) {
+        return "Equilateral triangle";
+    }
+
+    if (a === b || b === c || a === c) {
+        return "Isosceles triangle";
+    }
+
+    return "Scalene triangle";
+}
+const sideA = parseFloat(prompt("Enter the length of side A:"));
+const sideB = parseFloat(prompt("Enter the length of side B:"));
+const sideC = parseFloat(prompt("Enter the length of side C:"));
+
+const triangleType = checkTriangle(sideA, sideB, sideC);
+console.log(triangleType);
+checkTriangle()
+
+// NO#17
+function calCulate(selling ,cost) {
+    if ( selling > cost) {
+        let profit = selling - cost;
+        console.log(`profit is ${profit}`);
+    } else if ( selling < cost) {
+        let loss = cost - selling;
+        console.log(`loss is ${loss}`);
+    } 
+} 
+const selling = parseFloat(prompt("enter selling price"));
+const cost = parseFloat(prompt("enter cost price"));
+ const t = calCulate(selling ,cost) ;
+ console.log(t);
+ calCulate()
+ // NO#19
+ function calmarks(marks) {
+    const totalSubject = marks.length;
+    const totalMarks = marks.reduce((a, mark)   => a + mark, 0);
+    const percentage = (totalMarks / (totalSubject * 100))*100;
+    let grade;
+    if (percentage >= 90) {
+        grade = "A"
+    } else if ( percentage >= 80) {
+        grade = "B"
+    } else if (percentage >= 70) {
+        grade = "C"
+    } else if (percentage >= 60) {
+        grade = "D"
+    } else if (percentage >= 40) {
+        grade = "E"
+    } else  {
+        grade = "F"
+    }
+
+    return (
+        {percentage ,grade}
+    )
+
+ }
+ const physics = parseFloat(prompt("Enter marks for Physics:"));
+const chemistry = parseFloat(prompt("Enter marks for Chemistry:"));
+const biology = parseFloat(prompt("Enter marks for Biology:"));
+const mathematics = parseFloat(prompt("Enter marks for Mathematics:"));
+const computer = parseFloat(prompt("Enter marks for Computer:"));
+
+const marks = [physics, chemistry, biology, mathematics, computer];
+const F = calmarks(marks);
+console.log(`Percentage: ${F.percentage}`);
+console.log(`grade: ${F.grade}`);
+
+// calmarks()
+// NO#20
+function Salary(basicSalary) {
+    let hra ,da;
+    if ( basicSalary  <= 10000 ) {
+        hra= basicSalary * 0.20;
+        da = basicSalary * 0.80;
+    } else if (basicSalary  <= 20000) {
+        hra= basicSalary * 0.25;
+        da = basicSalary * 0.90;
+    } else if ( basicSalary > 20000 ) {
+        hra= basicSalary * 0.30;
+        da = basicSalary * 0.95;
+    } else {
+        hra= basicSalary * 0.30;
+        da = basicSalary * 0.95;    
+    }
+    const grossSalary = basicSalary + hra + da;
+    return  grossSalary ;
+}
+const basicSalary = parseFloat(prompt("Enter your basic salary"));
+const glossSalary =  Salary(basicSalary);
+console.log(glossSalary);
+Salary();
+// NO#21
+function billCheck( units) {
+let billAmount = 0;
+if ( units < 50) {
+    billAmount = units *0.50;
+} else if (billAmount < 100) {
+    billAmount = (50 * 0.50) + (units - 50) *0.75; 
+} else if ( units < 250) {
+    billAmount = (50 * 0.50) + ( 100* 0.75) + (units - 150) * 1.20;
+} else {
+    billAmount = (50 * 0.50) + (100 * 0.75) + (100 * 1.20 ) + (units - 250) * 1.50 ;
+}
+const surCharge = billAmount * 0.20;
+const totalBill = billAmount + surCharge;
+return totalBill;
+}
+const units = parseFloat(prompt("enter your unites"));
+const billfinal = billCheck(units);
+console.log(`your total bill is Rs: ${billfinal.toFixed(2)}`);
+billCheck()
+
+
+
